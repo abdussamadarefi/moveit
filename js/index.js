39a6ca7d8ebf7bd7903cb4ba325ab525ff54343a
abdussamadarefi <abdussamadaref@gmail.com>
2025-02-28 21:57:23 +0600

    implement Swiper JS functionality and enhance CSS styles for improved layout and responsiveness

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
-import "../scss/style.scss"; // SCSS
+// Author: Abdus Samad Arefi
+// Date: 2025-02-24
 
-console.log("Sass is working!");
+
+
+// Swiper JS
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    reverseDirection: true
+  },
+  loop: true,
+
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+      0:{
+          slidesPerView: 1
+      },
+      480:{
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      640:{
+          slidesPerView: 4,
+          spaceBetween: 20,
+      }
+  },
+
+});
